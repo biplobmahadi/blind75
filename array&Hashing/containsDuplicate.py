602ea714a56bfdddbc11e78ce2a35dcd0d157370
biplob <biplobhasan547@gmail.com>
2023-09-24 10:05:26 +0600

    add optimal soln for contains duplicate

--- array&Hashing/containsDuplicate.py
+++ array&Hashing/containsDuplicate.py
@@ -4,3 +4,10 @@ def containsDuplicate(nums: list):
             if nums[i] == nums[j]:
                 return True
     return False
+
+def containsDuplicateOptimal(nums: list):
+    hashset = set()
+    for n in nums:
+        if n in hashset: return True
+        hashset.add(n)
+    return False
