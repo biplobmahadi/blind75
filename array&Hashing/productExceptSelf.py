a3d6715d7c30965cbf586184f32fa86f10bb372a
biplob <biplobhasan547@gmail.com>
2023-09-27 17:55:57 +0600

    add another soln

--- array&Hashing/productExceptSelf.py
+++ array&Hashing/productExceptSelf.py
@@ -7,3 +7,23 @@ def productExceptSelf(nums):
                 product *= nums[j]
         ans.append(product)
     return ans
+
+def productExceptSelf2(nums):
+    ans, all = [], 1
+    zeros = []
+    for i, n in enumerate(nums):
+        if n == 0: zeros.append(i)
+        else: all *= n
+    if len(zeros) > 1:
+        ans = [0]*len(nums)
+    else:
+        for i in nums:
+            if len(zeros) ==1:
+                if i == 0:
+                    product = all
+                    ans.append(product)
+                else: ans.append(0)
+            else: 
+                product = all / i
+                ans.append(product)
+    return ans
