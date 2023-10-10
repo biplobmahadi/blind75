69f15070abfe99ffa9567855e60bc6f92c977ea1
biplob <biplobhasan547@gmail.com>
2023-10-10 23:58:45 +0600

    add optimal soln with binary search for find min

--- binarySearch/findMin.py
+++ binarySearch/findMin.py
@@ -4,3 +4,15 @@ def findMin(nums):
         res = min(res, nums[i])
     return res
 
+def findMinOptimal(nums):
+    left, right, res = 0, len(nums) - 1, nums[0]
+    while right>=left:
+        mid = (left + right) // 2
+        res = min(res, nums[mid])
+        if nums[mid] > nums[right]:
+            left = mid +1
+        else:
+            right = mid - 1
+    return res
+
+print(findMinOptimal([6, 1, 2, 3, 4, 5]))
