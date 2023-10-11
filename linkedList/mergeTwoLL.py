bab2cbf297b465833ae42f0b0361a8724fc46517
biplob <biplobhasan547@gmail.com>
2023-10-11 23:24:14 +0600

    add simple soln

--- linkedList/mergeTwoLL.py
+++ linkedList/mergeTwoLL.py
@@ -35,3 +35,21 @@ def mergeTwoLL(list1, list2):
                 tail = head
             list2 = list2.next
     return head
+
+
+def mergeTwo(list1, list2):
+    head = tail = ListNode() # suppose I have given this
+    while list1 and list2:
+        if list2.val >= list1.val:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next
+
+    if list1:
+        tail.next = list1
+    if list2:
+        tail.next = list2
+    return head.next
