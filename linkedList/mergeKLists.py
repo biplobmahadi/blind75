667e816c52aa1c71bb04e051d331ce73d4acdcba
biplob <biplobhasan547@gmail.com>
2023-10-14 12:33:08 +0600

    add anotehr soln

--- linkedList/mergeKLists.py
+++ linkedList/mergeKLists.py
@@ -17,3 +17,32 @@ def mergeKLists(lists):
             tail = n
         del hashMap[minVal]
     return head.next
+
+# O(nlogk) and O(k)
+def mergeKLists2(lists):
+    if not lists or len(lists) == 0:
+        return None
+    while len(lists)>1:
+        mergedList = []
+        for i in range(len(lists), 2):
+            l1 = lists[i]
+            l2 = lists[i+1] if i+1<len(lists) else None
+            mergedList.append(merge2(l1, l2))
+        lists = mergedList
+    return lists[0]
+
+def merge2(l1, l2):
+    head = tail = ListNode()
+    while l1 and l2:
+        if l1.val > l2.val:
+            tail.next = l2
+            l2 = l2.next
+        else:
+            tail.next = l1
+            l1 = l1.next
+        tail = tail.next
+    if l1:
+        tail.next = l1
+    if l2:
+        tail.next = l2
+    return head.next
