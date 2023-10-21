51984b18145e654c2b16f3edadc61517bc60b988
biplob <biplobhasan547@gmail.com>
2023-10-21 21:55:59 +0600

    check sub tree

---
+++ tree/isSubTree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+def isSameTree(p, q):
+    if not p and not q: 
+        return True
+    if not p or not q: 
+        return False
+    if p.val != q.val: 
+        return False
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+class Solution:
+    def isSubtree(self, root, subRoot) -> bool:
+        if not subRoot: return True
+        if not root: return False
+
+        if isSameTree(root, subRoot):
+            return True
+        return (self.isSubtree(root.left, subRoot) or 
+                self.isSubtree(root.right, subRoot))
