b53b3188bc20e72ba7dd9ae03d27a7006c211a8a
biplob <biplobhasan547@gmail.com>
2023-10-28 00:01:28 +0600

    update soln

--- backtracking/wordSearch.py
+++ backtracking/wordSearch.py
@@ -33,26 +33,26 @@ board = [["C","A","A"],["A","A","A"],["B","C","D"]]
 word = "AAB"
 print(exist(board, word))
 
-# def exist2(board, word):
-#     visited = [[False]*len(board[0]) for _ in range(len(board))]
-#     def dfs(i, row, col):
-#         if i == len(word):
-#             return True
-#         if (row<0 or col<0 or row>=len(board) or 
-#             col>=len(board[0]) or visited[row][col] or 
-#             board[row][col] != word[i]):
-#             return False
-#         visited[row][col] = True
-#         res = (dfs(i+1, row-1, col) or
-#             dfs(i+1, row, col+1) or
-#             dfs(i+1, row+1, col) or
-#             dfs(i+1, row, col-1))
-#         visited[row][col] = False
-#         return res
-#     for p in range(len(board)):
-#         for q in range(len(board[0])):
-#             if dfs(0, p, q):
-#                 return True
-#     return False
+def exist2(board, word):
+    visited = [[False]*len(board[0]) for _ in range(len(board))]
+    def dfs(i, row, col):
+        if i == len(word):
+            return True
+        if (row<0 or col<0 or row>=len(board) or 
+            col>=len(board[0]) or visited[row][col] or 
+            board[row][col] != word[i]):
+            return False
+        visited[row][col] = True
+        res = (dfs(i+1, row-1, col) or
+            dfs(i+1, row, col+1) or
+            dfs(i+1, row+1, col) or
+            dfs(i+1, row, col-1))
+        visited[row][col] = False
+        return res
+    for p in range(len(board)):
+        for q in range(len(board[0])):
+            if dfs(0, p, q):
+                return True
+    return False
 
-# print(exist2(board, word))
+print(exist2(board, word))
