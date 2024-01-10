0431fba839a7247b114ff2b921f318ef58dd3cc8
biplob <biplobhasan547@gmail.com>
2024-01-10 10:18:08 +0600

    add bottom up optimization

--- dp/climbingStairs.py
+++ dp/climbingStairs.py
@@ -1,8 +1,23 @@
+dp = {}
 def climb(n):
     if n == 0:
         return 1
     if n<0:
         return 0
-    return climb(n-1) + climb(n-2)
+    if n in dp:
+        return dp[n]
+    dp[n] = climb(n-1) + climb(n-2)
+    return dp[n]
 
 print(climb(3))
+
+def bottomUp(n):
+    dpOne = 1
+    dpTwo = 1
+    for _ in range(2, n+1):
+        dp = dpOne+dpTwo
+        dpOne = dpTwo
+        dpTwo = dp
+    return dpTwo
+
+print(bottomUp(3))
