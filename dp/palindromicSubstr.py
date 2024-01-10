b89b555fda7b9bd939be245cfe06d9bd78d87b6f
biplob <biplobhasan547@gmail.com>
2024-01-11 00:25:02 +0600

    add palindromic substr

---
+++ dp/palindromicSubstr.py
@@ -0,0 +1,16 @@
+def palindromicSubstr(s: str) -> str:
+    total = 0
+    for i in range(len(s)):
+        l, r = i, i
+        while l>=0 and r<len(s) and s[l] == s[r]:
+            total += 1
+            l-=1
+            r+=1
+        l, r = i, i+1
+        while l>=0 and r<len(s) and s[l] == s[r]:
+            total += 1
+            l-=1
+            r+=1
+    return total
+
+print(palindromicSubstr('aaa'))
