88a86fb0faf2f092fff1687c7c2c6765e876e30c
biplob <biplobhasan547@gmail.com>
2024-01-10 17:15:16 +0600

    add true dp soln

--- dp/houseRobber.py
+++ dp/houseRobber.py
@@ -11,3 +11,18 @@ def robber(n, nums, dp):
     return dp[n]
 
 print(rob([1,7,9,2]))
+
+def rob2(nums):
+    N = len(nums)
+    if N == 1: return nums[0]
+    if N == 2: return max(nums[0], nums[1])
+
+    one, two, three = 0, nums[0], nums[1]
+    for i in range(2, N):
+        res = nums[i] + max(one, two)
+        one = two
+        two = three
+        three = res
+    return max(two, three)
+
+print(rob2([1,7,9,2]))
