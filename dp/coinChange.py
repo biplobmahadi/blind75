bbc6da46636369e15772de22da0b38e7b96970a9
biplob <biplobhasan547@gmail.com>
2024-01-12 12:31:20 +0600

    add coin chagne soln

--- dp/coinChange.py
+++ dp/coinChange.py
@@ -1,15 +1,18 @@
 import math
 
 def topDown(coins, amount):
-    res = [math.inf]
-    def dfs(i, c, a):
-        if i == len(coins): return
-        if a == 0: res[0] = min(res[0], c)
-        dfs(i+1, c, a)
-        if a - coins[i] >= 0:
-            dfs(i, c+1, a - coins[i])
-            
-    dfs(0, 0, amount)
-    return res[0] if res[0] != math.inf else -1
+    if amount < 1: return 0
+    dp = {}
+    def dfs(a):
+        if a in dp: return dp[a]
+        if a == 0: return 0
+        val = math.inf
+        for n in coins:
+            if a - n >= 0:
+                val = min(val, dfs(a - n))
+        dp[a] = val + 1
+        return dp[a]
+    dfs(amount)
+    return dp[amount] if dp[amount] != math.inf else -1
 
 print(topDown([1], 0))
