83d43c7839bddabdfda0588b3c3bbbe35e2297f1
biplob <biplobhasan547@gmail.com>
2024-01-24 21:02:35 +0600

    solve coin change problem

--- dp/coinChange.py
+++ dp/coinChange.py
@@ -16,3 +16,17 @@ def topDown(coins, amount):
     return dp[amount] if dp[amount] != math.inf else -1
 
 print(topDown([1], 0))
+
+def bottomUp(coins, amount):
+    if amount < 1: return 0
+    dp = [math.inf] * (amount+1)
+    dp[0] = 0
+    for a in range(1, amount+1):
+        val = math.inf
+        for c in coins:
+            if a - c>= 0:
+                val = min(dp[a-c], val)
+        dp[a] = val + 1
+    return dp[amount] if dp[amount] != math.inf else -1
+
+print(bottomUp([1], 0))
