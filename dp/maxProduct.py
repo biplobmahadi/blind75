295125b318f9d60ad0eb1825ffd57f92993915cc
biplob <biplobhasan547@gmail.com>
2024-01-24 22:51:04 +0600

    add optimal soln

--- dp/maxProduct.py
+++ dp/maxProduct.py
@@ -10,3 +10,16 @@ def maxProduct(nums):
     return res
 
 print(maxProduct([2, 3, -2, 4]))
+
+def maxProductOptimal(nums):
+    res = nums[0]
+    currMax, currMin = 1, 1
+
+    for n in nums:
+        tmp = currMax * n
+        currMax = max(tmp, currMin*n, n)
+        currMin = min(tmp, currMin*n, n)
+        res = max(res, currMax)
+    return res
+
+print(maxProductOptimal([2, 3, -2, 4]))
