2f912197d9f70507c711d73b60cc066206860cc7
biplob <biplobhasan547@gmail.com>
2024-01-25 22:10:36 +0600

    use cache in the topdown soln

--- dp/wordBreak.py
+++ dp/wordBreak.py
@@ -1,11 +1,19 @@
 def wordBreak(s, wordDict):
-    return rec(0, s, wordDict)
+    dp = {}
+    return rec(0, s, wordDict, dp)
 
-def rec(i, s, wordDict):
-    if i == len(s): return True
+def rec(i, s, wordDict, dp):
+    if i == len(s): 
+        dp[i] = True
+        return True
+    if i in dp:
+        return dp[i]
     for j in range(i, len(s)):
         if s[i:j+1] in wordDict:
-            if (rec(j+1, s, wordDict)): return True
+            if (rec(j+1, s, wordDict, dp)): 
+                dp[i] = True
+                return True
+    dp[i] = False
     return False
 
-print(wordBreak('leetcod', ['leet', 'code']))
+print(wordBreak('leetcode', ['leet', 'code']))
