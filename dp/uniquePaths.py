2ca499366774e928230182582852f71e25ee530d
biplob <biplobhasan547@gmail.com>
2024-01-27 23:20:49 +0600

    add bottom up

--- dp/uniquePaths.py
+++ dp/uniquePaths.py
@@ -12,3 +12,14 @@ def uniq(m, n):
     return paths(0, 0)
 
 print(uniq(3, 7))
+
+def uniqPaths(m, n):
+    dp = [[0]*(n+1) for _ in range(m+1)]
+    dp[m-1][n-1] = 1
+    for r in range(m-1, -1,-1):
+        for c in range(n-1, -1,-1):
+            if dp[r][c] == 0:
+                dp[r][c] = dp[r+1][c] + dp[r][c+1]
+    return dp[0][0]
+
+print(uniqPaths(3, 7))
