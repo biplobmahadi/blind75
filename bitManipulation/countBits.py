840d1077b73bd4ee894eda7f1d71a55e79ac429b
biplob <biplobhasan547@gmail.com>
2024-02-03 23:03:51 +0600

    add dp soln of counting bits

--- bitManipulation/countBits.py
+++ bitManipulation/countBits.py
@@ -12,3 +12,14 @@ def countBits(n):
     return res
 
 print(countBits(5))
+
+def countBits2(n):
+    dp = [0] * (n+1)
+    offset = 1
+    for i in range(1, n+1):
+        if offset * 2 == i:
+            offset = i
+        dp[i] = 1 + dp[i-offset]
+    return dp
+
+print(countBits2(5))
