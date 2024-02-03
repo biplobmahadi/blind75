73776a8a01ab2ad79e4d940211bd44f0f5cd8f41
biplob <biplobhasan547@gmail.com>
2024-02-03 21:02:50 +0600

    add optimal soln to set zero in matrix

--- math&Geometry/setZeros.py
+++ math&Geometry/setZeros.py
@@ -14,3 +14,23 @@ def setZeros(matrix):
             matrix[i][c] = 0
         for i in range(r-1, -1, -1):
             matrix[i][c] = 0
+
+def setZero(matrix):
+    ROWS, COLS = len(matrix), len(matrix[0])
+    zeroRow = False
+    for r in range(ROWS):
+        for c in range(COLS):
+            if matrix[r][c] == 0:
+                matrix[0][c] = 0
+                if r>0: matrix[r][0] = 0
+                else: zeroRow = True
+    for r in range(1, ROWS):
+        for c in range(1, COLS):
+            if matrix[0][c] == 0 or matrix[r][0] == 0:
+                matrix[r][c] = 0
+    if matrix[0][0] == 0:
+        for r in range(ROWS):
+            matrix[r][0] = 0
+    if zeroRow:
+        for c in range(COLS):
+            matrix[0][c] = 0
